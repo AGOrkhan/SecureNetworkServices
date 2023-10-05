e3a35d5f2eed7b7ba7550b847fd03eefe5165e82
AGOrkhan <upperfloor2003@gmail.com>
2023-10-05 14:48:45 +0100

    Work
    Added class requirments

--- src/wk02_socketsendreceive/SocketClient_bare.java
+++ src/wk02_socketsendreceive/SocketClient_bare.java
@@ -2,12 +2,15 @@ package wk02_socketsendreceive;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class SocketClient_bare {
     public static void main( String[] args ) throws IOException {
          
     	int portNumber = 17777;
-    	String hostName = "127.0.0.1";
+        Scanner Temp = new Scanner(System.in);
+        System.out.println("Enter IP adress:");
+        String hostName = Temp.nextLine();
     	
         Socket s = new Socket( hostName, portNumber );
         
@@ -20,6 +23,10 @@ public class SocketClient_bare {
         while ( true ) {
         	message = userInput.readLine();	//reads keyboard until user hits a newline
             out.println( message );
+            if (message.contains("exit")){
+                s.close();
+                System.exit(0);
+            }
         }
     }
 }
--- src/wk02_socketsendreceive/SocketServer_bare.java
+++ src/wk02_socketsendreceive/SocketServer_bare.java
@@ -7,13 +7,20 @@ public class SocketServer_bare {
     public static void main( String[] args ) throws IOException {
          
     	int portNumber = 17777;
-    	
+
     	ServerSocket ss = new ServerSocket( portNumber );
     	Socket s = ss.accept();
         BufferedReader in = new BufferedReader( new InputStreamReader( s.getInputStream() ) );
-
-        while ( true ) {
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        if (s.isBound()){
+            System.out.println( "Incoming connection:"+ s.getInetAddress()+":"+s.getPort());
+        }
+        while (s.isConnected()) {
             System.out.println( "recv: " + in.readLine() );
+            String message = userInput.readLine();
+            if (message.contains("exit")){
+                s.close();
+            }
+        }
         }
     }
-}
